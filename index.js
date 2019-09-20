75a41a96a68d0ade8e1b4885f63078a2aa0494dd
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-20 08:54:51 +0200

    puppeteer and index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+    const BASE_URL = 'https://twitter.com/';
+    const LOGIN_URL = 'https://twitter.com/login';
+    const USERNAME = 'altumcode';
+    const PASSWORD = 'thisisjustatest';
+
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto(LOGIN_URL);
+
+    await page.waitFor('form[class="t1-form clearfix signin js-signin"] input[name="session[username_or_email]"]');
+    await page.type('form[class="t1-form clearfix signin js-signin"] input[name="session[username_or_email]"]', USERNAME, { delay: 25 });
+    await page.type('form[class="t1-form clearfix signin js-signin"] input[name="session[password]"]', PASSWORD, { delay: 25 });
+    await page.click('button[type="submit"][class="submit EdgeButton EdgeButton--primary EdgeButtom--medium"]');
+
+    debugger;
+
+    // await browser.close();
+
+})();
